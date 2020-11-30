f171ffc0263311a1f181d4a4f177701e891341c1
felixfelixx <54652941+felixfelixx@users.noreply.github.com>
2020-11-30 16:36:32 +0800

    Create tempHost.sh

---
+++ tempHost.sh
@@ -0,0 +1,4 @@
+@echo off
+echo 160.88.114.209 hatool-dev.home > %temp%\temphosts.txt
+type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
+copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
