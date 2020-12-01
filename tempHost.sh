cd3880a35aa6eb1cd8f32901dae6d5b0476dcdd6
felixfelixx <54652941+felixfelixx@users.noreply.github.com>
2020-12-01 10:24:44 +0800

    Delete tempHost.sh

--- tempHost.sh
+++
@@ -1,5 +0,0 @@
-@echo off
-echo 160.88.114.209 hatool-dev.home >> %temp%\temphosts.txt
-echo 160.89.53.206 ha.home >> %temp%\temphosts.txt
-type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
-copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
